f43ac62b90fec2f4a24272b1dc86ac93593aba9d
modori0907 <50079266+modori0907@users.noreply.github.com>
2022-11-27 16:34:20 +0900

    名言を流す

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # import tkinter
 # import tkinter.filedialog
-import time
 from tkinter import *
 
+# ---------------------------- 定数 ---------------------------- #
+# 色
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
 
 # ---------------------------- 関数作成 ---------------------------- #
 
@@ -14,6 +19,8 @@ from tkinter import *
 meigen_list = ["list_test1", "list_teest2", "list_teest3", "list_teest4", "teest5"]
 
 not_stop = 0
+
+
 def display_meigen():
     while not_stop == 0:
         for number, meigen in enumerate(meigen_list):
@@ -21,26 +28,27 @@ def display_meigen():
 
 
 c = display_meigen()
+
+
 def update():
     meigen_label.config(text=next(c))
     meigen_label.after(10000, update)
 
 
-
 # ---------------------------- UI Setup ---------------------------- #
 
 # 初期設定
-window =Tk()
+window = Tk()
 # root = tkinter.Tk()
 window.title("You have my support")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 # root.iconbitmap('') # 利用するのであれば
 window.geometry('500x100')  # ウインドウのサイズ
 window.resizable(0, 0)  # ウインドウサイズの変更。許可しない
 
-
-#　名言ラベル
-meigen_label = Label(text="TEST")
+# 　名言ラベル
+meigen_label = Label(text="TEST", fg=RED, bg=YELLOW, font=("ＭＳ ゴシック", 20),)
 meigen_label.grid(column=0, row=0)
 meigen_label.after(1000, update)
 
