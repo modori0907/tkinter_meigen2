cd7a960994805adaffbd4e352d9431d309348175
modori0907 <50079266+modori0907@users.noreply.github.com>
2022-12-11 20:06:57 +0900

    参考

--- tkinter_名言を出すアプリv2/test.py
+++ tkinter_名言を出すアプリv2/test.py
@@ -1,5 +1,19 @@
-test_list = [1,
-             2,
-             3,
-             ]
+import json
+
+
+coins = [
+    {
+        "symbol" : "BTC",
+        "amount_owned": 2,
+        "price_per_coin": 3200
+    },
+    {
+        "symbol": "BTS",
+        "amount_owned": 4,
+        "price_per_coin": 6200
+    }
+]
+
+for coin in coins:
+    print(coin["price_per_coin"])
 
